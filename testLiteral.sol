32354dddf681c8be19323cd72abf4ebe3754af12
emmett <xilibi2003@gmail.com>
2018-07-29 20:37:02 +0800

    update

--- testLiteral.sol
+++ testLiteral.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.18;
 
 contract testLiteral{
-    function interLiteral() public pure returns (uint, uint) {
-        return ((2**800 + 1) - 2**800,  0.5*8);
+
+    function test() public pure returns (int ) {
+        return -2e10;
     }
 
     function test2() public pure returns (uint ) {
@@ -10,10 +11,22 @@ contract testLiteral{
         return a;
     }
 
+    function testSoBig() public pure returns (uint) {
+        return ((2**800 + 1) - 2**800);
+    }
+
     // 数字常量表达式，一旦其中含有非常量表达式，它就会被转为一个非常量类型，不同类型的之间没法进行运算，
     function test3() public {
         uint128 a = 1;   // a 不再是常量类型
-        uint128 b = 2.5 + a + 0.5;
+          uint128 b = 2.5 + a + 0.5;
+    }
+
+    function testEn() public pure returns (string) {
+        return "abc";
+    }
+
+    function testChinese() public pure returns (string) {
+        return "登链科技";
     }
 
     function testString() public pure returns (string) {
@@ -21,7 +34,7 @@ contract testLiteral{
         return "Tiny\u718A";
     }
 
-    function hexLiteralBytes() public returns (bytes2, bytes1, bytes1) {
+    function hexLiteralBytes() public pure returns (bytes2, bytes1, bytes1) {
      bytes2 a = hex"aabb";
      return (a, a[0], a[1]);
  }
--- testenum.sol
+++ testenum.sol
@@ -1,24 +1,24 @@
 pragma solidity ^0.4.0;
 
-contract test {
+contract TestEnum {
 
     // 使用枚举自定义一个类型 ActionChoices
     enum ActionChoices { GoLeft, GoRight, GoStraight, SitStill }
 
     // 定义一个ActionChoices类型的变量
     ActionChoices choice;
-    
-    ActionChoices constant defaultChoice = ActionChoices.GoStraight;
+
+    ActionChoices defaultChoice = ActionChoices.GoStraight;
 
     function setGoStraight() {
         choice = ActionChoices.GoStraight;
     }
-    
+
     function getChoice() returns (ActionChoices) {
         return choice;
     }
 
-    function getDefaultChoice() returns (uint) {
+    function getDefaultChoice() public constant returns (uint) {
         return uint(defaultChoice);
     }
 }
